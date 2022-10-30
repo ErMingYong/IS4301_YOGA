49887e8ae2d56bfb613313ff499b307a821c47b8
NIL-99 <mingyong1999@gmail.com>
2022-10-30 23:11:20 +0800

    Editing of App.js with routings, refactoring of Dashbaord.js and Login.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login";
+import Login from "./pages/Login/Login.js";
+import Home from "./pages/Home/Home.js";
 import Register from "./Register";
 import Reset from "./Reset";
 import Dashboard from "./Dashboard";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Login />} />
+					<Route exact path="/home" element={<Home />} />
+					<Route exact path="/register" element={<Register />} />
+					<Route exact path="/reset" element={<Reset />} />
+					<Route exact path="/dashboard" element={<Dashboard />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,43 +5,48 @@ import "./Dashboard.css";
 import { auth, db, logout } from "./firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
 
+import Header from "./components/Header/Header.js";
+
 function Dashboard() {
-  const [user, loading, error] = useAuthState(auth);
-  const [name, setName] = useState("");
-  const navigate = useNavigate();
+	const [user, loading, error] = useAuthState(auth);
+	const [name, setName] = useState("");
+	const navigate = useNavigate();
 
-  const fetchUserName = async () => {
-    try {
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
+	const fetchUserName = async () => {
+		try {
+			const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+			const doc = await getDocs(q);
+			const data = doc.docs[0].data();
 
-      setName(data.name);
-    } catch (err) {
-      console.error(err);
-      alert("An error occured while fetching user data");
-    }
-  };
+			setName(data.name);
+		} catch (err) {
+			console.error(err);
+			alert("An error occured while fetching user data");
+		}
+	};
 
-  useEffect(() => {
-    if (loading) return;
-    if (!user) return navigate("/");
+	useEffect(() => {
+		if (loading) return;
+		if (!user) return navigate("/");
 
-    fetchUserName();
-  }, [user, loading]);
+		fetchUserName();
+	}, [user, loading]);
 
-  return (
-    <div className="dashboard">
-      <div className="dashboard__container">
-        Logged in as
-        <div>{name}</div>
-        <div>{user?.email}</div>
-        <button className="dashboard__btn" onClick={logout}>
-          Logout
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<div className="dashboard">
+				<div className="dashboard__container">
+					Logged in as
+					<div>{name}</div>
+					<div>{user?.email}</div>
+					<button className="dashboard__btn" onClick={logout}>
+						Logout
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Dashboard;
